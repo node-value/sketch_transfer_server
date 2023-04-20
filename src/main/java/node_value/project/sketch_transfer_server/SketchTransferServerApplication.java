699d69a88d0e82d6f989d565f372b715e781945e
node-value <gumangi3579@gmail.com>
2023-04-20 15:44:00 +0400

    Initialize Sping project

---
+++ src/main/java/node_value/project/sketch_transfer_server/SketchTransferServerApplication.java
@@ -0,0 +1,13 @@
+package node_value.project.sketch_transfer_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchTransferServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchTransferServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/node_value/project/sketch_transfer_server/SketchTransferServerApplicationTests.java
@@ -0,0 +1,13 @@
+package node_value.project.sketch_transfer_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchTransferServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

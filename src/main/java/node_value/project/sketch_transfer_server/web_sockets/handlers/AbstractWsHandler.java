6dd5a7f673fbef6bb2cde1942799b0101483142f
node-value <gumangi3579@gmail.com>
2023-04-23 21:39:33 +0400

    Handlers refactor

--- src/main/java/node_value/project/sketch_transfer_server/web_sockets/handlers/AbstractWsHandler.java
+++ src/main/java/node_value/project/sketch_transfer_server/web_sockets/handlers/AbstractWsHandler.java
@@ -1,10 +1,6 @@
 package node_value.project.sketch_transfer_server.web_sockets.handlers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.SubProtocolCapable;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,39 +10,14 @@ import node_value.project.sketch_transfer_server.service.JwtService;
 import java.util.Collections;
 import java.util.List;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 public abstract class AbstractWsHandler extends TextWebSocketHandler implements SubProtocolCapable {
 
-    private static final Logger logger = LoggerFactory.getLogger(CheckWSHandler.class);
-    
-    protected final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-    
     @Autowired JwtService jwtService;
 
     protected String extractUsername(WebSocketSession session) {
         return jwtService.extractUsername(session.getHandshakeHeaders().getFirst("cookie").split("=")[1]);
     }
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        super.supportsPartialMessages();
-        logger.info("Server connection opened on uri " + session.getUri().getPath()); 
-        sessions.put(extractUsername(session), session);
-        session.sendMessage(new TextMessage("User with name \"" + extractUsername(session) + "\" connected to server"));
-    }
-    
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        logger.info("Server connection closed: {}", status);
-        sessions.remove(extractUsername(session));
-    }
         
-    @Override
-    public void handleTransportError(WebSocketSession session, Throwable exception) {
-        logger.info("Server transport error: {}", exception.getMessage());
-    }
-    
     @Override
     public List<String> getSubProtocols() {
         return Collections.singletonList("subprotocol.demo.websocket");
--- src/main/java/node_value/project/sketch_transfer_server/web_sockets/handlers/AddWSHandler.java
+++ src/main/java/node_value/project/sketch_transfer_server/web_sockets/handlers/AddWSHandler.java
@@ -1,5 +1,9 @@
 package node_value.project.sketch_transfer_server.web_sockets.handlers;
 
+import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -8,7 +12,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import node_value.project.sketch_transfer_server.dto.ProjectDataDTO;
 
 public class AddWSHandler extends AbstractWsHandler {
-      
+    
+    private static final Logger logger = LoggerFactory.getLogger(AddWSHandler.class);
+    
+    protected final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception { 
         ObjectMapper mapper = new ObjectMapper();       
@@ -20,5 +28,23 @@ public class AddWSHandler extends AbstractWsHandler {
             data.setData("FAILED");
             session.sendMessage(new TextMessage(mapper.writeValueAsString(data)));
         }
+    }
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.supportsPartialMessages();
+        logger.info("Server connection opened on uri " + session.getUri().getPath()); 
+        sessions.put(extractUsername(session), session);
+        session.sendMessage(new TextMessage("User with name \"" + extractUsername(session) + "\" connected to server"));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        logger.info("Server connection closed: {}", status);
+        sessions.remove(extractUsername(session));
+    }
+        
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) {
+        logger.info("Server transport error: {} {}", exception.getMessage(), exception.getCause());
     }
 }
